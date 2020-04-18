c996f2ae8f228b7d7fd5bc26c80689056a766414
harshitkyal <harshitkyal@gmail.com>
2020-04-18 18:05:05 +0530

    File Upload

--- nodeJsServer/app.js
+++ nodeJsServer/app.js
@@ -8,6 +8,8 @@ var session = require('express-session');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
+const uploadRouter = require('./routes/uploadRouter');
+
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 connect.then((db) => {
@@ -50,7 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
-
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
